a14adcf344d2376d3dfa6f1ce3bb487ed900ba3f
Ahdaan Hashim <ahdaanhashim67@gmail.com>
2024-05-14 11:33:50 +0530

    leetcode 141 solution (has cycle)

---
+++ linkedlist/leetcode/141.go
@@ -0,0 +1,20 @@
+package leetcode
+
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+// func hasCycle(head *ListNode) bool {
+// 	visitedNode := make(map[*ListNode]bool)
+// 	current := head
+
+// 	for current != nil {
+// 		if visitedNode[current] {
+// 			return true
+// 		}
+// 		visitedNode[current] = true
+// 		current = current.Next
+// 	}
+// 	return false
+// }
