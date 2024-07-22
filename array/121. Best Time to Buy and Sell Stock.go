b49ffdcb6717f2545353f085657bb8939aa4c737
Ahdaan Hashim <ahdaanhashim67@gmail.com>
2024-07-23 03:35:26 +0530

    leetcode 121 best time to buy and sell stock  and 217 contain duplicate

---
+++ array/121. Best Time to Buy and Sell Stock.go
@@ -0,0 +1,16 @@
+package main
+// func maxProfit(prices []int) int {
+//     maxProfit, buyAmount := 0,prices[0]
+
+//     for _,price := range prices{
+//         if price < buyAmount{
+//             buyAmount = price
+//         } else {
+//             profit := price - buyAmount
+//             maxProfit = max(maxProfit,profit)
+//         }
+//     }
+
+//     return maxProfit
+    
+// }
---
+++ array/217. Contains Duplicate.go
@@ -0,0 +1,12 @@
+package main
+// func containsDuplicate(nums []int) bool {
+//     mp := make(map[int]bool)
+
+//     for _,n := range nums{
+//         if _,exist := mp[n]; exist {
+//             return true
+//         } 
+//         mp[n] = true
+//     }
+//     return false
+// }
